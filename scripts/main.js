1477bd4ff2d581d013fc9da95e5dabbc501b727d
Henry <henrykurniawan1st@gmail.com>
2023-01-20 23:54:22 +0700

    Update homepage var

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 const { app, globalShortcut, BrowserWindow, screen } = require('electron');
 const path = require('path');
 
-var homePage = 'https://andaf.proofhub.com/';
+var homePage = 'https://xyz.proofhub.com/'; // Put your company's ProofHub URL here
 
 var userAgent =
   'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.101 Safari/537.36'; // Linux
